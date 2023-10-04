a8c42f5fb5f57aaf2baf4f3a5f82492c45fe2e14
RedFreak_ <llwsw@gmx.de>
2023-10-04 12:01:16 +0200

    test: fixed base Testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,45 +2,9 @@
 
 namespace RedFreak\Menu\Tests;
 
-use Orchestra\Testbench\Concerns\CreatesApplication;
-use Orchestra\Testbench\PHPUnit\TestCase as BaseTestCase;
-use RedFreak\Modules\ModulesServiceProvider;
-use Illuminate\Contracts\Foundation\Application;
-use RedFreak\Modules\Tests\Behaviour\DoesNotCreateApplication;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
-abstract class TestCase extends BaseTestCase
+abstract class TestCase extends OrchestraTestCase
 {
-    use CreatesApplication;
 
-    /** @var \Illuminate\Foundation\Application|null */
-    protected static ?Application $appStore = null;
-
-    protected ?Application $app = null;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        if (!in_array(DoesNotCreateApplication::class, class_uses($this), true)) {
-            if (!self::$appStore) {
-                self::$appStore = $this->createApplication();
-            }
-
-            $this->app = self::$appStore;
-        }
-    }
-
-    /**
-     * Get package providers.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     *
-     * @return array<int, class-string<\Illuminate\Support\ServiceProvider>>
-     */
-    protected function getPackageProviders($app)
-    {
-        return [
-            ModulesServiceProvider::class
-        ];
-    }
 }
