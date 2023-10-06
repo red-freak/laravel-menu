8f096cf25005551255c5e1f3a7c79b3e21d4a823
RedFreak_ <llwsw@gmx.de>
2023-10-06 09:10:17 +0200

    test: incommented tests again

--- tests/Unit/MenuRenderTest.php
+++ tests/Unit/MenuRenderTest.php
@@ -4,27 +4,27 @@ use RedFreak\Menu\Item;
 use RedFreak\Menu\MenuManager;
 use RedFreak\Menu\RenderOptions;
 
-//it('can render a menu without set config', function () {
-//    $menu = app(MenuManager::class)->add('testMenu');
-//    expect($menu->render())->toBe('<ul class="menu menu-level-0">'.PHP_EOL.'</ul>'.PHP_EOL);
-//});
-//
-//it('can append name and type information to the menu', function () {
-//    config()->set('menus.classes.add_type_to_root', true);
-//
-//    $menu = app(MenuManager::class)->add('testMenu', [RenderOptions::KEY_STYLE => 'menu']);
-//    expect($menu->render())->toBe('<ul class="menu menu--menu menu-level-0">'.PHP_EOL.'</ul>'.PHP_EOL);
-//    $menu = app(MenuManager::class)->add('testMenu');
-//    expect($menu->render())->toBe('<ul class="menu menu--list menu-level-0">'.PHP_EOL.'</ul>'.PHP_EOL);
-//
-//    config()->set('menus.classes.add_menu_as_class', true);
-//    expect($menu->render())->toBe('<ul class="menu menu--list menu--testMenu menu-level-0">'.PHP_EOL.'</ul>'.PHP_EOL);
-//});
-//
-//it('registers a render-macro for a menu', function() {
-//    app(MenuManager::class)->add('testMenu');
-//    expect(MenuManager::testMenu())->toBe('<ul class="menu menu-level-0">'.PHP_EOL.'</ul>'.PHP_EOL);
-//});
+it('can render a menu without set config', function () {
+    $menu = app(MenuManager::class)->add('testMenu');
+    expect($menu->render())->toBe('<ul class="menu menu-level-0">'.PHP_EOL.'</ul>'.PHP_EOL);
+});
+
+it('can append name and type information to the menu', function () {
+    config()->set('menus.classes.add_type_to_root', true);
+
+    $menu = app(MenuManager::class)->add('testMenu', [RenderOptions::KEY_STYLE => 'menu']);
+    expect($menu->render())->toBe('<ul class="menu menu--menu menu-level-0">'.PHP_EOL.'</ul>'.PHP_EOL);
+    $menu = app(MenuManager::class)->add('testMenu');
+    expect($menu->render())->toBe('<ul class="menu menu--list menu-level-0">'.PHP_EOL.'</ul>'.PHP_EOL);
+
+    config()->set('menus.classes.add_menu_as_class', true);
+    expect($menu->render())->toBe('<ul class="menu menu--list menu--testMenu menu-level-0">'.PHP_EOL.'</ul>'.PHP_EOL);
+});
+
+it('registers a render-macro for a menu', function() {
+    app(MenuManager::class)->add('testMenu');
+    expect(MenuManager::testMenu())->toBe('<ul class="menu menu-level-0">'.PHP_EOL.'</ul>'.PHP_EOL);
+});
 
 it('can render a menu with a label as translation key', function () {
     app()->translator->addLines(['*.testItem' => 'testItem_Translated'], 'en');
